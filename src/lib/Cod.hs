c6e60e2f9901b88b66c25158bb8cc0a70a0ea19f
Taylor Fausak <taylor@fausak.me>
2020-09-12 10:34:25 -0400

    Allow passing in language extensions

--- src/lib/Cod.hs
+++ src/lib/Cod.hs
@@ -10,6 +10,7 @@ import qualified ErrUtils
 import qualified FastString
 import qualified GHC
 import qualified GHC.Hs
+import qualified GHC.LanguageExtensions.Type
 import qualified GHC.Paths
 import qualified HeaderInfo
 import qualified Lexer
@@ -22,10 +23,11 @@ type Errors = Bag.Bag ErrUtils.ErrMsg
 type Module = SrcLoc.Located (GHC.Hs.HsModule GHC.Hs.GhcPs)
 
 parse
-  :: FilePath
+  :: [GHC.LanguageExtensions.Type.Extension]
+  -> FilePath
   -> Data.ByteString.ByteString
   -> IO (Either Errors Module)
-parse filePath byteString = do
+parse extensions filePath byteString = do
   let libdir = Just GHC.Paths.libdir
   dynFlags1 <- GHC.runGhc libdir GHC.getSessionDynFlags
 
@@ -37,15 +39,17 @@ parse filePath byteString = do
   let fastString = FastString.mkFastString filePath
   let realSrcLoc = SrcLoc.mkRealSrcLoc fastString 1 1
 
-  let locatedStrings = HeaderInfo.getOptions dynFlags1 stringBuffer filePath
-  (dynFlags2, _, _) <- DynFlags.parseDynamicFilePragma dynFlags1 locatedStrings
+  let dynFlags2 = foldr (flip DynFlags.xopt_set) dynFlags1 extensions
 
-  let pState1 = Lexer.mkPState dynFlags2 stringBuffer realSrcLoc
+  let locatedStrings = HeaderInfo.getOptions dynFlags2 stringBuffer filePath
+  (dynFlags3, _, _) <- DynFlags.parseDynamicFilePragma dynFlags2 locatedStrings
+
+  let pState1 = Lexer.mkPState dynFlags3 stringBuffer realSrcLoc
 
   pure $ case Lexer.unP Parser.parseModule pState1 of
-    Lexer.PFailed pState2 -> Left . snd $ Lexer.getMessages pState2 dynFlags2
+    Lexer.PFailed pState2 -> Left . snd $ Lexer.getMessages pState2 dynFlags3
     Lexer.POk pState2 locatedHsModuleGhcPs ->
-      let bagErrMsg = snd $ Lexer.getMessages pState2 dynFlags2
+      let bagErrMsg = snd $ Lexer.getMessages pState2 dynFlags3
       in if null bagErrMsg
         then Right locatedHsModuleGhcPs
         else Left bagErrMsg
--- src/test/Main.hs
+++ src/test/Main.hs
@@ -6,6 +6,7 @@ import qualified Cod
 import qualified Control.Monad as Monad
 import qualified Data.ByteString as ByteString
 import qualified Data.Either as Either
+import qualified GHC.LanguageExtensions.Type as X
 import qualified System.Exit as Exit
 import qualified Test.HUnit as Test
 
@@ -18,26 +19,35 @@ main = do
 tests :: Test.Test
 tests = Test.TestList
   [ Test.TestLabel "handles invalid UTF-8 without throwing" $ expectLeft
+    []
     "\x80"
   , Test.TestLabel "fails to parse an invalid module" $ expectLeft
+    []
     "module invalid where"
   , Test.TestLabel "parses a valid module" $ expectRight
+    []
     "module Valid where"
   , Test.TestLabel "fails to parse without required extension" $ expectLeft
+    []
     "module WithoutBlockArguments where\n\
     \zero = id do 0"
   , Test.TestLabel "parses with required extension" $ expectRight
+    []
     "{-# language BlockArguments #-}\n\
     \module WithBlockArguments where\n\
     \zero = id do 0"
+  , Test.TestLabel "parses with required extension passed in" $ expectRight
+    [X.BlockArguments]
+    "module WithBlockArguments where\n\
+    \zero = id do 0"
   ]
 
-expectLeft :: ByteString.ByteString -> Test.Test
-expectLeft contents = Test.TestCase $ do
-  result <- Cod.parse "" contents
+expectLeft :: [X.Extension] -> ByteString.ByteString -> Test.Test
+expectLeft extensions contents = Test.TestCase $ do
+  result <- Cod.parse extensions "" contents
   Test.assertBool "expected Left but got Right" $ Either.isLeft result
 
-expectRight :: ByteString.ByteString -> Test.Test
-expectRight contents = Test.TestCase $ do
-  result <- Cod.parse "" contents
+expectRight :: [X.Extension] -> ByteString.ByteString -> Test.Test
+expectRight extensions contents = Test.TestCase $ do
+  result <- Cod.parse extensions "" contents
   Test.assertBool "expected Right but got Left" $ Either.isRight result
